3fd3242082fb71b5634b8c34eb16955bb3e886a3
fangwei716 <fangwei455916930@gmail.com>
2016-02-17 23:19:15 -0700

    bug fixed

--- readme.md
+++ readme.md
@@ -6,3 +6,11 @@ Require node.js & xcode
 1. `npm install`
 
 2. `run DNA.xcodeproj`
+
+To do:
+
+1. updates
+2. edit profile
+3. submit form
+4. handle multiple forms
+5. sign up info
--- view/store.js
+++ view/store.js
@@ -356,7 +356,7 @@ const styles = StyleSheet.create({
   blur:{
     position: 'absolute',
     top: 0, bottom: 0, left: 0, right: 0,
-    backgroundColor:"rgba(0,0,0,0.25)"
+    backgroundColor:"rgba(0,0,0,0.15)"
   },
   noblur:{
     position:"absolute",
